1681f4c2f6e270f6dae4ea26ef42f8fb6ee2058a
John Fawcett <jrf0110@gmail.com>
2013-01-25 14:26:47 -0600

    fixed config for grunt

--- config.js
+++ config.js
@@ -34,3 +34,80 @@ define(function(require){
     ]
   };
 });
+
+define(function(require){
+  var config = {
+    dev: {
+      apiUrl: 'http://localhost:3000/'
+
+    , cuisineTypes: [
+        'American'
+      , 'Asian'
+      , 'Fusion'
+      , 'Barbeque'
+      , 'Brazillian'
+      , 'Breakfast'
+      , 'Burgers'
+      , 'Cafes'
+      , 'Caribbean'
+      , 'Chinese'
+      , 'Cuban'
+      , 'Diners'
+      , 'Ethiopian'
+      , 'Greek'
+      , 'Indian'
+      , 'Italian'
+      , 'Japanese'
+      , 'Korean'
+      , 'Mediterranean'
+      , 'Mexican'
+      , 'Pizza'
+      , 'Sandwiches'
+      , 'Seafood'
+      , 'Sushi'
+      , 'Thai'
+      , 'Vegan'
+      , 'Vegetarian'
+      , 'Vietnamese'
+      ]
+    }
+
+  , prod: {
+      apiUrl: 'http://localhost:3000/'
+
+    , cuisineTypes: [
+        'American'
+      , 'Asian'
+      , 'Fusion'
+      , 'Barbeque'
+      , 'Brazillian'
+      , 'Breakfast'
+      , 'Burgers'
+      , 'Cafes'
+      , 'Caribbean'
+      , 'Chinese'
+      , 'Cuban'
+      , 'Diners'
+      , 'Ethiopian'
+      , 'Greek'
+      , 'Indian'
+      , 'Italian'
+      , 'Japanese'
+      , 'Korean'
+      , 'Mediterranean'
+      , 'Mexican'
+      , 'Pizza'
+      , 'Sandwiches'
+      , 'Seafood'
+      , 'Sushi'
+      , 'Thai'
+      , 'Vegan'
+      , 'Vegetarian'
+      , 'Vietnamese'
+      ]
+    }
+  };
+
+  // Build step will change this to prod
+  return config.dev;
+});
