12e64816bebe0e5d84b0b999f16bb339b86c901a
bmc <brian.m.carlson@gmail.com>
2013-06-07 13:35:02 -0500

    Work on integrating existing controls into dashboard

--- views/bizpanel/sections/dashboard.js
+++ views/bizpanel/sections/dashboard.js
@@ -2,9 +2,14 @@ define(function(require) {
   var Section = require('../section');
   var dashboard = new (Section.extend({
     template: require('hbt!../../../templates/bizpanel/dashboard'),
+    editTpl:require('hbt!../../../templates/page-business-details-location-edit'),
     icon: 'home',
     url: '#panel/dashboard',
-    text: 'Dashboard'
+    text: 'Dashboard',
+    render: function(data) {
+      this.$el.html(this.template(this.data||{}))
+      this.$el.html(this.editTpl(this.data.location || {}))
+    }
   }));
   return dashboard;
 });
