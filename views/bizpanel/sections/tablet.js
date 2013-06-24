ace2af41df53201eecf61fed19c6c63fbadc0a9b
Brian Carlson <brian.m.carlson@gmail.com>
2013-06-24 12:13:49 -0500

    initial layout of tablet gallery section

--- views/bizpanel/sections/tablet.js
+++ views/bizpanel/sections/tablet.js
@@ -1,39 +1,83 @@
 define(function(require) {
+  var _ = require('underscore');
   var bus = require('../../../lib/pubsub');
+  var MsgView = require('../msg-view');
   var Section = require('../section');
   var api = require('../../../lib/api');
   var utils = require('../../../lib/utils');
+  var ProductFilter = require('../../../lib/bizpanel/product-filter');
+
+  var GalleryOrderEditor = MsgView.extend({
+    initialize: function() {
+      this.productFilter = new ProductFilter({
+        hideUncategorized: true,
+        hideSpotlight: false
+      });
+      this.template = require('hbt!../../../templates/bizpanel/gallery-order-editor');
+    },
+    render: function(menu) {
+      if(!menu) return;
+      var viewData = {
+        products: this.productFilter.filterProducts(menu.products),
+        categories: this.productFilter.getCategorySelectView(menu.sections)
+      };
+      this.$el.html(this.template(viewData));
+    },
+    subscribe: {
+      saveCategoryOrderEdits: function() {
+        alert('not implemented')
+      }
+    }
+  });
 
   var tablet = new (Section.extend({
     template: require('hbt!../../../templates/bizpanel/tablet'),
     url: '#panel/tablet',
     icon: 'desktop',
     text: 'Tablet Gallery',
-    initialize: function() {
+    events: {
+      "change select#categorySelect": "selectedCategoryChanged"
     },
-    render: function(data) {
+    selectedCategoryChanged: function(e) {
+      var $el = $(e.target);
+      this.productFilter.setCategoryFilter($el.val());
+      this.render();
+    },
+    initialize: function() {
+      this.editor = new GalleryOrderEditor();
+      this.productFilter = new ProductFilter({
+        hideUncategorized: true,
+        hideSpotlight: true
+      });
+    },
+    render: function(menu) {
       console.log('section-tablet','render');
       var self = this;
-      this.$el.html(this.template(data || {}))
+      menu = menu || this.menu;
+      var viewData = {
+        products: [],
+        categories: []
+      };
+      if(menu) {
+        viewData.products = this.productFilter.filterProducts(menu.products);
+        viewData.categories = this.productFilter.getCategorySelectView(menu.sections);
+      }
+      this.$el.html(this.template(viewData));
+      this.editor.render(menu);
+      this.$el.find('#tablet-edit-container').html(this.editor.$el);
     },
     subscribe: {
-      loadManagerEnd: function(data) {
-        if(!data) return;
-        this.user = data.user;
-        var locationChange = (this.location||0).id != data.location.id;
-        this.location = data.location;
-        this.business = data.business;
-        if(locationChange) {
-          this.render();
-          //load menu on location change
-          var msg = {
-            businessId: data.business.id,
-            locationId: data.location.id
-          }
-        }
+      loadMenuEnd: function(menu) {
+        this.menu = menu;
+        this.render(menu);
       },
-      loadMenuEnd: function(menuData) {
-
+      editTabletOrder: function() {
+        this.$el.find('#tablet-view-container').hide();
+        this.$el.find('#tablet-edit-container').show();
+      },
+      cancelCategoryOrderEdits: function() {
+        this.$el.find('#tablet-edit-container').hide();
+        this.$el.find('#tablet-view-container').show();
       }
     }
   }));
