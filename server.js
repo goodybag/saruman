7c5ed434f47801f1900a704751357953ea89185a
Brian Carlson <brian.m.carlson@gmail.com>
2013-07-02 17:43:57 -0500

    Add config block to server

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ app.use(express.static(__dirname));
 
 var proxy = new httpProxy.RoutingProxy();
 
+app.configure(function() {
+  app.set('proxy.host', 'localhost');
+  app.set('proxy.port', '3000');
+});
+
+app.configure('staging', 'production', function() {
+  app.set('proxy.host', 'magic.staging.goodybag.com');
+});
+
 app.use(function(req, res, next) {
   proxy.proxyRequest(req, res, {
-    host: 'localhost',
-    port: 3000
+    host: app.set('proxy.host'),
+    port: app.set('proxy.port')
   });
 });
 
