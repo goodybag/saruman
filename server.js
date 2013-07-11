b50700cf7b0fbd5526460f3ae8bc9b5c349e80c4
Brian Carlson <brian.m.carlson@gmail.com>
2013-07-11 18:31:29 +0000

    add configuration to server

--- server.js
+++ server.js
@@ -3,25 +3,28 @@ var log = require('logged')(__filename);
 var app = require('express')();
 var httpProxy = require('http-proxy');
 
-app.use(express.static(__dirname));
 
 
 app.configure('development', function() {
   app.set('proxy.host', 'localhost');
   app.set('proxy.port', 3000);
+  app.set('listen.port', process.env.PORT || 3000);
+  app.set('dir.static', __dirname);
   app.use(express.static(__dirname + '/test'));
 });
 
-app.configure('staging', 'production', function() {
+app.configure('staging', function() {
   app.set('proxy.host', 'magic.staging.goodybag.com');
+  app.set('dir.static', __dirname + '/build');
+  app.set('listen.port', process.env.PORT || 8000);
   app.set('proxy.port', 80);
 });
 
 app.configure('production', function() {
-  app.configure('staging', 'production', function() {
-    app.set('proxy.host', 'magic.goodybag.com');
-    app.set('proxy.port', 80);
-  });
+  app.set('proxy.host', 'magic.goodybag.com');
+  app.set('dir.static', __dirname + '/build');
+  app.set('listen.port', process.env.PORT || 8001);
+  app.set('proxy.port', 80);
 });
 
 var proxy = new httpProxy.HttpProxy({
@@ -32,6 +35,7 @@ var proxy = new httpProxy.HttpProxy({
   changeOrigin: true
 });
 
+app.use(express.static(app.set('dir.static')));
 app.use(function(req, res, next) {
   log.debug('proxy request', {
     path: req.path,
@@ -41,7 +45,7 @@ app.use(function(req, res, next) {
   proxy.proxyRequest(req, res);
 });
 
-var port = process.env.PORT || 8000;
+var port = app.set('listen.port');
 require('http').createServer(app).listen(port, function() {
   log.info('listening', {port: port});
 });
