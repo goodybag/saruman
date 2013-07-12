80bb00fcb21bfa926153517edf0fd143b85c4451
Brian Carlson <brian.m.carlson@gmail.com>
2013-07-12 13:19:22 -0500

    Promote user to manager when adding user to manager group

--- models/user-list-item.js
+++ models/user-list-item.js
@@ -43,12 +43,28 @@ define(function(require){
         this.set('email', 'user-' + this.attributes.id + '@generated.goodybag.com')
       }
 
+    , checkForManagerModification: function(key, value) {
+        if(this.attributes.groups) {
+          if(key == 'groups') {
+            //if the user's group collection was modified with the
+            //'manager' group, automatically promote or demote as needed when
+            //the model is saved
+            var wasManager = _.findWhere(this.attributes.groups, {name: 'manager'});
+            var isManager = _.findWhere(value, {name: 'manager'});
+            this._promoteToManager = false;
+            if(!wasManager && isManager) {
+              this._promoteToManager = true;
+            }
+          }
+        }
+      }
+
     , set: function(key, value){
+        this.checkForManagerModification(key, value);
         if (typeof key === "object"){
           for (var k in key)
             if (this.acceptable.indexOf(k) === -1) delete key[k];
         } else if (this.acceptable.indexOf(key) === -1) return this;
-
         utils.Model.prototype.set.apply(this, arguments);
       }
 
@@ -68,23 +84,41 @@ define(function(require){
           delete attr.password;
         }
 
+        var doPromotion = function(cb) {
+          if(!this_._promoteToManager) return cb(null);
+          var id = this_.get('id');
+          if(this_._promoteToManager) {
+            console.log('promoting');
+            api.users.managers.update(id, {businessId: null, locationId: null}, cb);
+            return cb(null);
+          }
+        };
+
         if (this.attributes.id && this.attributes.id !== 'New'){
           if (attr.groups){
             for (var i = 0, l = attr.groups.length; i < l; ++i){
               delete attr.groups[i].metaTotal;
             }
           }
-          api.users.update(this.attributes.id, attr, callback);
-        }
-        else {
-          api.users.create(attr, function(error, result){
-            if (error) return callback && callback(error);
-
-            this_.set('id', result.id);
-
-            if (callback) callback(null, result)
+          api.users.update(this.attributes.id, attr, function(error, result, extra) {
+            if(error) return callback(error);
+            return doPromotion(function(error) {
+              if(error) return callback(error);
+              return callback(null, result, extra);
+            });
           });
         }
+        else {
+          api.users.create(attr, function(error, result, extra){
+            if (error) return callback && callback(error);
+            this_.set('id', result.id);
+            return doPromotion(function(error) {
+              if(error) return callback && callback(error);
+              return callback && callback(null, result, extra);
+            });
+          });
+        }
+
 
         return this;
       }
